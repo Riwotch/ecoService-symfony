9a0c779ad88e369b0eb69bc0b1fcb1ce1ae509a7
Valliere Alan <alan.valliere@laposte.net>
2021-03-04 22:35:22 +0100

    [FIX] Image display

--- src/Controller/Admin/AdminProductController.php
+++ src/Controller/Admin/AdminProductController.php
@@ -67,7 +67,6 @@ class AdminProductController extends AbstractController
                 $originalFilename = pathinfo($img->getClientOriginalName(), PATHINFO_FILENAME);
                 $safeFilename = $slugger->slug($originalFilename);
                 $newFilename = $safeFilename.'-'.uniqid().'.'.$img->guessExtension();
-
                 try {
                     $img->move(
                         $this->getParameter('uploadSource'),
